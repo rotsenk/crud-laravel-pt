081b4d6c9f788075942dfa8af81a13dfa44ac1b0
rotsenk <stanley.95@hotmail.com>
2025-02-26 08:53:45 -0600

    CRUD Proyecto Laravel

---
+++ app/Http/Controllers/PersonasController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Personas;
+use Illuminate\Http\Request;
+
+class PersonasController extends Controller
+{
+    public function index()
+    {
+        // página de inicio
+    }
+
+    public function create()
+    {
+        //  formulario donde agregaremos los datos
+    }
+
+    public function store(Request $request)
+    {
+        // sirve para guardar datos en la BD
+    }
+
+    public function show(Personas $personas)
+    {
+        // servirá para obtener un registro de nuestra tabla
+    }
+
+    public function edit(Personas $personas)
+    {
+        // método para traer datos a editar y los coloca en un formulario
+    }
+
+    public function update(Request $request, Personas $personas)
+    {
+        // este método actualiza los datos en la DB
+    }
+
+    public function destroy(Personas $personas)
+    {
+        // elimina un registro
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/plantilla')
+
+@section('tituloPagina', 'Crud Laravel')
+
+@section('contenido')
+    <div class="row">
+        <h1>Hello World</h1>
+        <i class="fas fa-wrench"></i>
+    </div>
+
+@endsection
